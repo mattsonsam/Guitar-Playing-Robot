1fdfaab3ee8419b7814bf4785e119b30593cccf3
Amanda Vasconcelos <avasconcelos28@gmail.com>
2021-11-02 17:48:54 -0400

    Update by Amanda 11.2.21
    Updated the code to better work with 2 steppers and two servos. Added some organization. Code compiles.

--- multistepper/multistepper.ino
+++ multistepper/multistepper.ino
@@ -1,47 +1,85 @@
-/*#include <AccelStepper.h>
+//-------------------------------------SETUP VARIABLES----------------------------------------------------------------------------------------------------
+#include <AccelStepper.h>
 #include <MultiStepper.h>
 #include <Servo.h>
 
-AccelStepper stepper(1,54,55); //1 is a default setting, 54 is the pin connected to the "DIR+" or direction pin on the driver, 55 is the pin connected to "PUL+" or pulse pin
-Servo frettingservo; //initialize servo                    
-long strokelengthmm = 400; //long for maximum travel distance of fretting rail in mm. Accel stepper requires that inputs to movement functions is long (i think)
-double step_per_rev=800; //number of steps per revolution of motor, given by stepper driver. Stepper drive has small DIP switches to select this
-double mm_per_rev = 72; //Exerimentally determined number of mm traveled per revolution. This is used to convert from mm distances to number of steps.
-double mmPerStep = mm_per_rev/step_per_rev; //conversion factor
-int switch1 = 3; //signal pin for home limit switch (near headstock)
-int switch2 = 9; //signal pin for end stop limit switch (near body)
-int pos=45; //variable to set initial position of frettingservo upon startup, in degrees, of the fretting servo
-void setup() { //runs once upon power up
+AccelStepper fretting(1,54,55); //using x-step pins
+AccelStepper strumming(1,60,61); //using y-step pins
+Servo muting;
+Servo majorminor;
+
+int strumhome=3; //pin for limit switch for strummer
+int frethome=14; //pin for limit switch for fretter
+
+long fret_strokelengthmm = 400; //the length of the entire fretting linear rail
+double fret_step_per_rev=800; // steps per revolution of stepper
+double fret_mm_per_rev = 72; //linear distance travelled in one rotation of stepper
+double fret_mmPerStep = fret_mm_per_rev/fret_step_per_rev; //linear distance travelled in one step of the stepper
+
+long strum_strokelengthmm; //these need to be determined
+double strum_step_per_rev;
+double strum_mm_per_rev;
+double strum_mmPerStep = strum_mm_per_rev/strum_step_per_rev;
+
+int majorminor_down = 0;
+int majorminor_up = 45;
+//int majorminor_angle = 45;
+int mute_down = 0; //this needs to be determined
+int mute_up = 45; //this needs to be determined 
+
+//------------------------------------SETUP FUNCTION-------------------------------------------------------------------------------------------------------
+void setup() { 
   Serial.begin(9600);  
-  pinMode(switch1,INPUT_PULLUP); //define limit switch pins as inputs, with internal pullup resistors used on arduino. Makes it so that no external resistor is needed with switch
-  pinMode(switch2,INPUT_PULLUP);
-  frettingservo.attach(11);  //defines signal pin for servo
-  frettingservo.write(pos);  //move servo to angle defined by pos (intended to be not pressing string at first)
-  stepper.setMaxSpeed(1000000.0); //set arbitrarily high max speed, in units of [pulses/sec] (max reliable is about 4000 according to accelstepper)
-  delay(1000); //wait for things to finish moving
-  gohome(); //do homing routine (see below)
-  
-  
+  pinMode(strumhome,INPUT_PULLUP); //sets two limit switches as inputs with internal resistors
+  pinMode(frethome,INPUT_PULLUP);
+  majorminor.attach(11);  //attaches majorminor servo to pin 11 
+  muting.attach(4);
+  servosUp();
+  fretting.setMaxSpeed(1000000.0); //set arbitrarily high max speed, in units of [pulses/sec] (max reliable is about 4000 according to accelstepper).Prevents speed from being limited by code, we will not use this
+  strumming.setMaxSpeed(1000000.0);
+  delay(1000); 
+  //goHome();  
+}
+//-------------------------------------END SETUP------------------------------------------------------------------
+
+
+//-------------------------------------START VOID LOOP------------------------------------------------------------
+
+void loop(){ //here is where we will call all our functions
+//  trapmotion(100,2);
 }
 
-void loop() {
-trapmotion(100,2);
+//------------------------------------END VOID LOOP---------------------------------------------------------------
 
- 
+
+//------------------------------------START FUNCTIONS-------------------------------------------------------------
+//---servos---
+void servosUp(){ //both servos up
+  majorminor.write(majorminor_up); 
+  muting.write(mute_up);
 }
-void gohome(){
-  long strokelength=ceil(strokelengthmm/mmPerStep); // convert stroke length from mm to number of steps. This way the carriage will always travel far enough to hit the home switch.
-  stepper.moveTo(strokelength);  //sets the target position in steps, of the stepper motor. It does not actually tell it to move yet. 
-  while(digitalRead(switch1)==0){  //while the switch is not pressed, do the following
-    stepper.setSpeed(-300); //setting the speed, with negative being towards the home switch. Setting the position in each instance of a loop, makes it travel at a constant speed. If is confusion talk to sam for a shitty explanation
-    stepper.run(); //Executes a step in each iteration of the loop. Does math to achieve desired speed and accel
-    //Serial.println(stepper.currentPosition());
+
+
+void servosChangingFrets(){
+  majorminor.write(majorminor_up); 
+  muting.write(mute_down);
+}
+
+//---go home----
+//Written by Amanda: gohome funct, will need to pass values in when called, may not be worth it to reduce # of functions by 1 to instead pass in values
+void goHome(long strokeLengthmm, long mmPerStep, AccelStepper stepper, int limitSwitch, int homingSpeed){
+  long strokelength=ceil(strokeLengthmm/mmPerStep); //calculates number of steps to traverse entire rail
+  stepper.moveTo(strokelength);
+
+  while(digitalRead(limitSwitch)==0){  
+    stepper.setSpeed(homingSpeed); 
+    stepper.run(); 
   }
   stepper.setCurrentPosition(0); //once home, set position to zero for reference
   delay(1000);
 }
-
-void gotoPositionAtSpeed(long posmm, float spedmm, float accelmm){ 
+//---go to position----
+  void gotoPositionAtSpeed(AccelStepper stepper, long posmm, long mmPerStep, float spedmm, float accelmm){ //function to tell the stepper to go to a  position at a specific speed with speed in units of mm/s
     long numSteps=ceil(posmm/mmPerStep);
     float Speed=spedmm/mmPerStep;
     float acceleration=accelmm/mmPerStep;
@@ -54,7 +92,8 @@ void gotoPositionAtSpeed(long posmm, float spedmm, float accelmm){
     }
 }
 
-void gotoPositionAtSpeedsteps(long posmm, float sped, float accel){
+
+void gotoPositionAtSpeedsteps(AccelStepper stepper, long mmPerStep, long posmm, float sped, float accel){ //function to tell the stepper to go to a position at a specific speed with speed in units of steps/s
     long numSteps=ceil(posmm/mmPerStep);
     //float Speed=spedmm/mmPerStep;
     //float acceleration=accelmm/mmPerStep;
@@ -67,7 +106,8 @@ void gotoPositionAtSpeedsteps(long posmm, float sped, float accel){
     }
 }
 
-void trianglemotion (long posmm,float t){ //plans and executes the triangular motion profile to get the carriage to the desired position in the desired amount of time, limited by the max velocity of 4000 steps/s
+//---motion math---
+void trianglemotion (long posmm, long mmPerStep, float t, AccelStepper stepper){ //plans and executes the triangular motion profile to get the carriage to the desired position in the desired amount of time, limited by the max velocity of 4000 steps/s
   long targetsteps=floor(posmm/mmPerStep); //number of steps needed to get to abolute desired position
   float v_avg= abs((stepper.currentPosition()-targetsteps)/t); //avg speed to get to desired position from current position
   float v_max= 2*v_avg; //max velocity assuming equal time accelerating and decelerating
@@ -82,7 +122,7 @@ void trianglemotion (long posmm,float t){ //plans and executes the triangular mo
   
 }
 
-void trapmotion( long posmm, float t){     // this function uses a trapezoidal motion profile with equal times of acceleration, constant speed, and deceleration. from this link: 
+void trapmotion( long posmm, long mmPerStep, float t, AccelStepper stepper){     // this function uses a trapezoidal motion profile with equal times of acceleration, constant speed, and deceleration. from this link: 
   long targetsteps=floor(posmm/mmPerStep); //number of steps needed to get to abolute desired position
   float accel = (4.5*(stepper.currentPosition()-targetsteps))/pow(t,2);   //calc accel needed
   float v_max = (1.5*(stepper.currentPosition()-targetsteps))/t;       //calc max (target) velocity
@@ -94,7 +134,14 @@ void trapmotion( long posmm, float t){     // this function uses a trapezoidal m
   }
 }
 
-void gotoangle(int a){
+//-----------------------------------------END FUNCTIONS--------------------------------------------------------------
+
+
+//-----------------------------------------EXPERIMENTAL---------------------------------------------------------------
+/**/
+//---------------------------DEFUNCT----------------------
+//functions that likely are defunction but good to keep around in case we need to reference then later, can be added to a library of old stuff at some point to clean this up
+/*void gotoangle(int a){ //this is unneeded as servo.write goes to a position and does not need to worry about delay
   //pos=90;
   while (frettingservo.read()!=a){
     if(a>frettingservo.read()){
@@ -106,61 +153,32 @@ void gotoangle(int a){
     frettingservo.write(pos);
     delay(15);
   }
-}*/
-
-#include <AccelStepper.h>
-#include <MultiStepper.h>
-#include <Servo.h>
-
-AccelStepper fretting(1,54,55); //using x-step pins
-AccelStepper strumming(1,60,61); //using y-step pins
-Servo muting;
-Servo majorminor;
-
-int strumhome=3;
-int frethome=14;
-
-long fret_strokelengthmm = 400; 
-double fret_step_per_rev=800; 
-double fret_mm_per_rev = 72; 
-double fret_mmPerStep = fret_mm_per_rev/fret_step_per_rev;
-
-long strum_strokelengthmm;
-double strum_step_per_rev;
-double strum_mm_per_rev;
-double strum_mmPerStep =  strum_mm_per_rev/strum_step_per_rev;
-
-int majorminor_angle = 45;
-int mute_angle = 0;
-
-void setup() { 
-  Serial.begin(9600);  
-  pinMode(strumhome,INPUT_PULLUP); 
-  pinMode(frethome,INPUT_PULLUP);
-  majorminor.attach(11);  
-  majorminor.write(majorminor_angle);  
-  muting.attach(4);
-  muting.write(mute_angle);
-  stepper.setMaxSpeed(1000000.0); //set arbitrarily high max speed, in units of [pulses/sec] (max reliable is about 4000 according to accelstepper)
-  delay(1000); 
-  gohome();  
 }
 
-void gohome(){
-  long fret_strokelength=ceil(fret_strokelengthmm/fret_mmPerStep);
-  long strum_strokelength=ceil(strum_strokelengthmm/strum_mmPerStep);
 
+void fretGoHome(){
+  long fret_strokelength=ceil(fret_strokelengthmm/fret_mmPerStep); //calculates number of steps to traverse entire rail
+  
   fretting.moveTo(fret_strokelength);
-  strumming.moveTo(strum_strokelength);
-  
-  
-}
-  /*long strokelength=ceil(strokelengthmm/mmPerStep); 
-  stepper.moveTo(strokelength);  
-  while(digitalRead(switch1)==0){  
-    stepper.setSpeed(-300); 
-    stepper.run(); 
+
+  while(digitalRead(frethome)==0){  
+    fretting.setSpeed(-300); 
+    fretting.run(); 
   }
-  stepper.setCurrentPosition(0); //once home, set position to zero for reference
+  fretting.setCurrentPosition(0); //once home, set position to zero for reference
   delay(1000);
 }
+
+
+void strumGoHome(){ //need to adjust set speed and direction to properly approach limit switch
+  long strum_strokelength=ceil(strum_strokelengthmm/strum_mmPerStep); //calculates number of steps to traverse entire rail
+  strumming.moveTo(strum_strokelength);
+
+  while(digitalRead(strumhome)==0){  
+    strumming.setSpeed(-300); 
+    strumming.run(); 
+  }
+  fretting.setCurrentPosition(0); //once home, set position to zero for reference
+  delay(1000);
+}
+*/
